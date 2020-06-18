223aaca1e6af4e00f46697e0b91a8ebb5ebb26be
khachatur.tovmasyan@gmail.com <khachatur.tovmasyan@gmail.com>
2020-06-18 22:06:57 +0400

    Setup React

--- build.sh
+++ build.sh
@@ -11,3 +11,20 @@ tsc --sourceMap false &&
 cd ./build
 
 NODE_ENV=production node start.js
+
+### Bundle FrontEnd ###
+
+# Create the directory for React
+mkdir -p ./build/public/app/
+
+# Navigate to the react directory
+cd ./src/public/app
+
+# Build React code
+npm run build
+
+# Rename the folder
+mv build app
+
+# Move the contains to the build/ dir
+mv app ../../../../build/public/
