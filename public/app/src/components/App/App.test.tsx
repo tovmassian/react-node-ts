0a8b5dfc478170d8d9aa0d65f5823c90e2fe1bf8
khachatur.tovmasyan@gmail.com <khachatur.tovmasyan@gmail.com>
2020-06-23 18:55:42 +0400

    Only back-end setup

--- public/app/src/components/App/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/save to reload/i);
-    expect(linkElement).toBeInTheDocument();
-});
--- util/startDevFull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### Run Back-End ###
-
-nodemon --config './util/nodemon.json' &
-
-### Run Front-End ###
-
-npm start --prefix public/app
