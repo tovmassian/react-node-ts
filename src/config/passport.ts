a5eb65a2aeefbe29435f992f89575d7c8233dc17
khachatur.tovmasyan@gmail.com <khachatur.tovmasyan@gmail.com>
2020-06-26 20:49:05 +0400

    Create Use schema and use it when signing up with Google/Picsart

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,5 +1,6 @@
 import * as passport from 'passport';
 import { Profile, Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import User, { IUser } from '../models/User';
 import './dotEnv';
 
 const { GOOGLE_CLIENT_ID, GOOGLE_SECRET, GOOGLE_CALLBACK_URL } = process.env;
@@ -12,7 +13,16 @@ passport.use(
             callbackURL: GOOGLE_CALLBACK_URL || '',
         },
         (accessToken: string, refreshToken: string, profile: Profile) => {
-            console.log(profile);
+            if (profile._json.hd === 'picsart.com') {
+                try {
+                    // creating Model Instance and save to DB
+                    new User({ googleId: profile.id }).save();
+                } catch (e) {
+                    console.log(e.message);
+                }
+            } else {
+                throw Error('Please use your PicsArt account for authorization');
+            }
         },
     ),
 );
