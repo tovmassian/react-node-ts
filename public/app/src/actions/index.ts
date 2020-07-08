d9a4150fa3e9a89b91d632ed1aef54f673f367d9
khachatur.tovmasyan@gmail.com <khachatur.tovmasyan@gmail.com>
2020-07-08 11:24:15 +0400

    remove console.log

--- public/app/src/actions/index.ts
+++ public/app/src/actions/index.ts
@@ -1,7 +1,6 @@
 import { FETCH_USER } from './types';
 
 const fetchUser = () => async (dispatch: any) => {
-    console.log('fetching user....');
     dispatch({
         type: FETCH_USER,
         payload: await fetch('/api/user').then((res) => res.ok && res.json()),
